bb7d22c71daf91a854ff8a4e1e92e30fd9ea3500
Kotori <543566812@qq.com>
2020-04-19 13:09:35 +0800

    [Task] Blog - Initial commit

---
+++ K_Blog/src/com/Kotori/web/LoginAction.java
@@ -0,0 +1,10 @@
+package com.Kotori.web;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class LoginAction extends ActionSupport {
+    public String Login(){
+        System.out.println("Login");
+        return null;
+    }
+}
